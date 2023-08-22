fdf4889e3b28bccd0416b95fafb3a0d4989029d1
HopeFullee <john_lee0607@naver.com>
2023-08-22 13:47:01 +0900

    style: 커스텀 인풋 ~ 텍스트에리아 컴포넌트 작성

--- src/app/write/page.tsx
+++ src/app/write/page.tsx
@@ -3,6 +3,7 @@
 import Link from "next/link";
 import { useState } from "react";
 import CustomInput from "@/components/shared/CustomInput";
+import CustomTextArea from "@/components/shared/CustomTextArea";
 
 type InputElements = HTMLInputElement | HTMLTextAreaElement;
 
@@ -12,14 +13,32 @@ const WritePage = () => {
     content: "",
   });
 
-  const [alertState, setAlertState] = useState();
+  const [regexState, setRegexState] = useState({
+    title: false,
+    content: false,
+  });
 
   const handleChange = (e: React.ChangeEvent<InputElements>) => {
     const { name, value } = e.target;
     setForumData((prev) => ({ ...prev, [name]: value }));
+
+    if (value.trim() === "") {
+      setRegexState((prev) => ({ ...prev, [name]: true }));
+    } else {
+      setRegexState((prev) => ({ ...prev, [name]: false }));
+    }
   };
 
-  const forumSubmitHanlder = (e: React.FormEvent<HTMLFormElement>) => {};
+  const forumSubmitHanlder = (e: React.FormEvent<HTMLFormElement>) => {
+    Object.entries(forumData).forEach(([key, value]) => {
+      if (value.trim() === "") {
+        e.preventDefault();
+        setRegexState((prev) => ({ ...prev, [key]: true }));
+      } else {
+        setRegexState((prev) => ({ ...prev, [key]: false }));
+      }
+    });
+  };
 
   return (
     <div>
@@ -31,7 +50,7 @@ const WritePage = () => {
           method="POST"
           className="w-full gap-40 mt-40 flex-col-center"
         >
-          <ul className="flex justify-between w-full">
+          <div className="flex justify-between w-full">
             <label htmlFor="forum_title" className="font-semibold w-50">
               제목
             </label>
@@ -40,24 +59,26 @@ const WritePage = () => {
               name="title"
               type="text"
               placeholder="글 제목"
+              onChange={(e) => handleChange(e)}
               value={forumData.title}
-              onChange={(e) => handleChange(e)}
-              alert={"제목을 입력해주세요"}
-            />
-          </ul>
-          <div className="flex justify-between w-full">
-            <label htmlFor="forum_content" className="font-semibold">
-              내용
-            </label>
-            <textarea
-              id="forum_content"
-              name="content"
-              onChange={(e) => handleChange(e)}
-              value={forumData.content}
-              className="w-[90%] outline-none resize-none p-5 min-h-150"
+              regexMessage={"제목을 입력해주세요"}
+              regexState={regexState.title}
             />
           </div>
-
+          <div className="flex justify-between w-full">
+            <label htmlFor="forum_content" className="font-semibold w-50">
+              내용
+            </label>
+            <CustomTextArea
+              id="forum_content"
+              name="content"
+              placeholder="본문 내용"
+              onChange={(e) => handleChange(e)}
+              value={forumData.content}
+              regexMessage={"내용을 입력해주세요"}
+              regexState={regexState.content}
+            />
+          </div>
           <div className="flex justify-end w-full gap-20">
             <Link href={"/list"}>
               <button
@@ -67,7 +88,6 @@ const WritePage = () => {
                 취소
               </button>
             </Link>
-
             <button
               type="submit"
               className="px-20 py-3 font-semibold border-black border-1 text-14"
--- src/components/shared/CustomInput/index.tsx
+++ src/components/shared/CustomInput/index.tsx
@@ -1,16 +1,19 @@
 "use client";
 
 interface Props extends React.ComponentProps<"input"> {
-  alert: string;
+  regexState: boolean;
+  regexMessage: string;
 }
 
-const CustomInput = ({ alert, ...rest }: Props) => {
+const CustomInput = ({ regexState, regexMessage, ...rest }: Props) => {
   return (
     <div className="relative w-full">
       <input {...rest} className="w-full p-5 outline-none" />
-      <p className="absolute bottom-[-25px] left-5 text-red-500 text-13">
-        {alert}
-      </p>
+      {regexState && (
+        <p className="absolute bottom-[-25px] left-5 text-red-500 text-13">
+          {regexMessage}
+        </p>
+      )}
     </div>
   );
 };
