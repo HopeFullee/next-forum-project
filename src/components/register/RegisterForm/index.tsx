9ce38d76dd14ada6e6fdde6498b57c1ac64d7b90
HopeFullee <john_lee0607@naver.com>
2023-09-09 22:06:12 +0900

    refactor: 전윤스승님의 은혜를 받아, 회원가입 submit HOOK 으로 변경 / submit 시 중복 이메일 있으면 서버 에러 받아와서 프론트 에러 표시 작업.

--- src/components/register/RegisterForm/index.tsx
+++ src/components/register/RegisterForm/index.tsx
@@ -1,162 +1,152 @@
-"use client";
-import { useState } from "react";
-import CustomInput from "@/components/shared/CustomInput";
-import Link from "next/link";
+'use client';
+import { useEffect, useState } from 'react';
+import CustomInput from '@/components/shared/CustomInput';
+import Link from 'next/link';
+import { useRegister } from '@/hooks/useRegister';
 
 const emailRe = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
 const passwordRe = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,15}$/;
 
+export interface RegisterData {
+  email: string;
+  password: string;
+  confirmPassword: string;
+}
+
 const RegisterForm = () => {
-  const [isSubmit, setIsSubmit] = useState(false);
+  const { isFetching, register, error } = useRegister();
 
   const [registerData, setRegisterData] = useState({
-    email: "",
-    password: "",
-    confirmPassword: "",
+    email: '',
+    password: '',
+    confirmPassword: '',
   });
 
   const [regexWarning, setRegexWarning] = useState({
-    email: "",
-    password: "",
-    confirmPassword: "",
+    email: '',
+    password: '',
+    confirmPassword: '',
   });
+
+  useEffect(() => {
+    setRegexWarning((prevState) => ({ ...prevState, ...error }));
+  }, [error]);
+
+  const errorSet = (formKey: string, errorMsg: string) => {
+    setRegexWarning((prev) => ({
+      ...prev,
+      [formKey]: errorMsg,
+    }));
+  };
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { value, name } = e.target;
     setRegisterData((prev) => ({ ...prev, [name]: value }));
 
     // if input field is null -> trigger regexState to show warning
-    if (value.trim() === "") {
-      return setRegexWarning((prev) => ({
-        ...prev,
-        [name]: "*필수 항목입니다.",
-      }));
+    if (value.trim() === '') {
+      errorSet(name, '*필수 항목입니다.');
     }
 
-    if (name === "email") {
-      if (!emailRe.exec(value)) {
-        setRegexWarning((prev) => ({
-          ...prev,
-          [name]: "*이메일 형식이 아닙니다.",
-        }));
-      } else {
-        setRegexWarning((prev) => ({ ...prev, [name]: "" }));
-      }
+    if (name === 'email') {
+      if (!emailRe.exec(value)) errorSet(name, '*이메일 형식이 아닙니다.');
+      else errorSet(name, '');
     }
 
-    if (name === "password") {
+    if (name === 'password') {
       if (!passwordRe.exec(value)) {
         setRegexWarning((prev) => ({
           ...prev,
-          [name]: "*8~15, 영문, 숫자, 특수기호 포함.",
+          [name]: '*8~15, 영문, 숫자, 특수기호 포함.',
         }));
       } else {
-        setRegexWarning((prev) => ({ ...prev, [name]: "" }));
+        setRegexWarning((prev) => ({ ...prev, [name]: '' }));
       }
     }
 
-    if (name === "confirmPassword") {
+    if (name === 'confirmPassword') {
       if (value !== registerData.password) {
         setRegexWarning((prev) => ({
           ...prev,
-          [name]: "*비밀번호가 일치하지 않습니다.",
+          [name]: '*비밀번호가 일치하지 않습니다.',
         }));
       } else {
-        setRegexWarning((prev) => ({ ...prev, [name]: "" }));
+        setRegexWarning((prev) => ({ ...prev, [name]: '' }));
       }
     }
   };
 
-  const handleSubmit = () => {
-    // check for empty fields
-    const tempState = { ...regexWarning };
+  const getEmptyFormKey = () => {
+    const emptyFormKeyList: string[] = [];
 
     Object.entries(registerData).forEach(([key, value]) => {
-      if (value.trim() === "") {
-        for (const property in tempState) {
-          if (property === key) {
-            (tempState as any)[property] = "*필수 항목입니다.";
-          }
-        }
-      }
+      if (value.trim() === '') emptyFormKeyList.push(key);
     });
 
-    setRegexWarning(tempState);
+    return emptyFormKeyList;
+  };
 
-    const isWarning = Object.entries(tempState).some(([_, warning]) => {
-      return warning !== "";
-    });
+  const handleSubmit = () => {
+    const emptyFormKey = getEmptyFormKey();
 
-    if (!isWarning) {
-      // disable submit button incase of SPAM click
-      setIsSubmit(true);
-      // if no regex warning then request to API
-      fetch("/api/auth/register", {
-        method: "POST",
-        body: JSON.stringify({
-          email: registerData.email,
-          password: registerData.password,
-          confirmPassword: registerData.confirmPassword,
-        }),
-      })
-        .then(() => {
-          // redirect user after submission
-          window.location.replace("/forum");
-        })
-        .catch((error) => {
-          // turn back on submit button if API connection has errors
-          setIsSubmit(false);
-          console.log(error);
-        });
+    emptyFormKey.forEach((formKey) =>
+      setRegexWarning((prevState) => ({
+        ...prevState,
+        [formKey]: '*필수 항목입니다.',
+      })),
+    );
+
+    if (emptyFormKey.length === 0) {
+      register(registerData);
     }
   };
 
   return (
-    <div className="mx-auto max-w-1400 flex-center">
+    <div className='mx-auto max-w-1400 flex-center'>
       <form
         onSubmit={(e) => e.preventDefault}
-        className="gap-30 mt-100 flex-col-center"
+        className='gap-30 mt-100 flex-col-center'
       >
         <p>회원가입</p>
         <div>
-          <label htmlFor="register_email">Email</label>
+          <label htmlFor='register_email'>Email</label>
           <CustomInput
-            id="register_email"
-            type="email"
-            name="email"
-            placeholder="이메일"
+            id='register_email'
+            type='email'
+            name='email'
+            placeholder='이메일'
             onChange={(e) => handleChange(e)}
             value={registerData.email}
             regexWarning={regexWarning.email}
           />
         </div>
         <div>
-          <label htmlFor="register_pw">Password</label>
+          <label htmlFor='register_pw'>Password</label>
           <CustomInput
-            id="register_pw"
-            type="password"
-            name="password"
-            placeholder="비밀번호"
+            id='register_pw'
+            type='password'
+            name='password'
+            placeholder='비밀번호'
             onChange={(e) => handleChange(e)}
             value={registerData.password}
             regexWarning={regexWarning.password}
           />
         </div>
         <div>
-          <label htmlFor="register_cpw">Confirm Password</label>
+          <label htmlFor='register_cpw'>Confirm Password</label>
           <CustomInput
-            id="register_cpw"
-            type="password"
-            name="confirmPassword"
-            placeholder="비밀번호 확인"
+            id='register_cpw'
+            type='password'
+            name='confirmPassword'
+            placeholder='비밀번호 확인'
             onChange={(e) => handleChange(e)}
             value={registerData.confirmPassword}
             regexWarning={regexWarning.confirmPassword}
           />
         </div>
-        <div className="flex justify-end w-full gap-20 mt-10 under:border-1 under:border-gray-500 under:px-5 under:py-1 under:rounded-sm">
-          <Link href="/">취소</Link>
-          <button disabled={isSubmit} type="button" onClick={handleSubmit}>
+        <div className='flex justify-end w-full gap-20 mt-10 under:border-1 under:border-gray-500 under:px-5 under:py-1 under:rounded-sm'>
+          <Link href='/'>취소</Link>
+          <button disabled={isFetching} type='button' onClick={handleSubmit}>
             회원가입
           </button>
         </div>
---
+++ src/hooks/useRegister.ts
@@ -0,0 +1,37 @@
+import { RegisterData } from '@/components/register/RegisterForm';
+import { useState } from 'react';
+
+export const useRegister = () => {
+  const [isFetching, setIsFetching] = useState(false);
+
+  const [error, setError] = useState<Record<string, string>>({});
+
+  const register = async (registerData: RegisterData) => {
+    setIsFetching(true);
+
+    try {
+      const res = await fetch('/api/auth/register', {
+        method: 'POST',
+        body: JSON.stringify({
+          email: registerData.email,
+          password: registerData.password,
+          confirmPassword: registerData.confirmPassword,
+        }),
+      });
+
+      if (res.ok) window.location.replace('/forum');
+      else throw res;
+    } catch (err: any) {
+      setIsFetching(false);
+
+      const errorBody = await err.json();
+      setError(errorBody);
+    }
+  };
+
+  return {
+    isFetching,
+    error,
+    register,
+  };
+};
