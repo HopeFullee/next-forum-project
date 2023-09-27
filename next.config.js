6b52834508a50c9af7bfd399947eac3f3c31ef46
HopeFullee <john_lee0607@naver.com>
2023-09-27 23:50:39 +0900

    feat: google Oauth 추가

--- next.config.js
+++ next.config.js
@@ -6,5 +6,7 @@ module.exports = {
   env: {
     GITHUB_APP_ID: process.env.GITHUB_APP_ID,
     GITHUB_APP_SECRET: process.env.GITHUB_APP_SECRET,
+    GOOGLE_APP_ID: process.env.GOOGLE_APP_ID,
+    GOOGLE_APP_SECRET: process.env.GOOGLE_APP_SECRET,
   },
 };
--- src/components/signin/LoginForm/ProviderForm/index.tsx
+++ src/components/signin/LoginForm/ProviderForm/index.tsx
@@ -1,24 +1,35 @@
 import Image from "next/image";
 import githubLogo from "@/assets/providerLogo/github.png";
+import googleLogo from "@/assets/providerLogo/google.png";
 import { signIn } from "next-auth/react";
 
 const ProviderForm = () => {
   return (
-    <>
+    <div>
       <div className="w-full gap-10 flex-center">
         <span className="w-full h-1 border-black border-dashed border-t-1"></span>
         <p className="font-medium">or</p>
         <span className="w-full h-1 border-black border-dashed border-t-1"></span>
       </div>
-      <div className="mt-5 flex-center">
-        <Image
-          src={githubLogo}
-          alt="github"
-          className="cursor-pointer w-65 h-65"
-          onClick={() => signIn("github")}
-        />
-      </div>
-    </>
+      <nav className="mt-20 gap-15 flex-center">
+        <div className="flex-center">
+          <Image
+            src={githubLogo}
+            alt="github"
+            className="cursor-pointer w-45 h-45"
+            onClick={() => signIn("github")}
+          />
+        </div>
+        <div className="flex-center">
+          <Image
+            src={googleLogo}
+            alt="google"
+            className="cursor-pointer w-45 h-45"
+            onClick={() => signIn("google")}
+          />
+        </div>
+      </nav>
+    </div>
   );
 };
 
