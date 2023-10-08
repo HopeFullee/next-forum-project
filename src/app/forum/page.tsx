da5353ab36cd4813e25f30d1ea3f443caed80e3f
HopeFullee <john_lee0607@naver.com>
2023-10-08 22:19:55 +0900

    feat: 포스트 작성 서버 날짜 프론트에서 보여주게 작업중

--- src/app/forum/page.tsx
+++ src/app/forum/page.tsx
@@ -14,9 +14,9 @@ const ForumPage = async () => {
   return (
     <div className="mx-auto max-w-1200">
       <ul className="under:w-full flex-col-center gap-30 mt-100">
-        {postList.map(({ _id, title, createdAt }, idx) => {
+        {postList.map(({ _id, title, createdAt }) => {
           return (
-            <ListCard key={idx} id={_id} title={title} createdAt={createdAt} />
+            <ListCard key={_id} id={_id} title={title} createdAt={createdAt} />
           );
         })}
       </ul>
--- src/components/list/ListCard/index.tsx
+++ src/components/list/ListCard/index.tsx
@@ -1,5 +1,8 @@
+"use client";
+
 import { ObjectId } from "mongodb";
 import Link from "next/link";
+import { useState, useEffect } from "react";
 
 type ListCardProps = {
   id: string;
@@ -8,11 +11,21 @@ type ListCardProps = {
 };
 
 const ListCard = ({ id, title, createdAt }: ListCardProps) => {
+  const [localTime, setLocalTime] = useState("");
+
+  useEffect(() => {
+    const date = new Date(createdAt);
+    const browserLocale = window.navigator.language;
+    const formatter = new Intl.DateTimeFormat(browserLocale);
+
+    setLocalTime(formatter.format(date));
+  });
+
   return (
     <Link href={`/detail/${id}`}>
       <li className="border-b-2 rounded-sm p-15 border-cyan-500/40">
         <h4 className="font-semibold text-20">{title}</h4>
-        <p>{createdAt}</p>
+        <p>{localTime}</p>
       </li>
     </Link>
   );
